2bdfa0b50b48760a85e8da4f15f6223681079fb5
Nurudeen Bada <nolanrewaju@gmail.com>
2021-01-25 21:13:26 +0000

    my typwriter text done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 
 //import components
 import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousel from './components/my-carousel/my-carousel.component';
+import TitleMessage from './components/title-message/title-message.component';
+
 
 const App = () => {
   return (
     <div className="App">
       <MyNavbar />
+      <MyCarousel />
+      <TitleMessage />
     </div>
   );
 }
