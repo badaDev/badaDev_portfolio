75ec47f93c0817b629d6f7a92f0200ebedeaf0b8
Nurudeen Bada <nolanrewaju@gmail.com>
2021-02-28 18:32:34 +0000

    experience section done

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import MyCarousel from './components/my-carousel/my-carousel.component';
 import TitleMessage from './components/title-message/title-message.component';
 import About from './pages/about/about.component';
 import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
 import { Parallax } from 'react-parallax';
 import Container from 'react-bootstrap/Container'
 import BgImage from './assets/img/parallex/background.webp'
 import Skills from './pages/skills/skills.components';
+import Experience from './pages/experience/experience.components';
 
 
 const App = () => {
@@ -39,12 +41,22 @@ const App = () => {
       <div>
         <div>
           <Container className="container-box rounded">
-            <Fade duration={500}>
+            <Slide bottom duration={500}>
+              <hr />
               <Skills />
-            </Fade>
+            </Slide>
           </Container>
         </div>
       </div>
+
+      <div>
+          <Container className="container-box rounded">
+            <Fade duration={500}>
+              <hr />
+              <Experience />
+            </Fade>
+          </Container>
+        </div>
     </div>
   );
 }
